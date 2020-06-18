2e8d8577239718f3f8909e26a51a3e469eacc61f
devkwon97 <kkm97721@gmail.com>
2020-06-19 02:36:50 +0900

    1093번

---
+++ src/com/company/CodeUp1093.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class CodeUp1093 {
+
+    public static void main(String[] args){
+
+        int total = 0;
+
+        for(int i=0; i<=5; i++){
+            for(int j=1; j<=i; i++){
+                total++;
+            }
+        }
+        System.out.print(total);
+    }
+}
