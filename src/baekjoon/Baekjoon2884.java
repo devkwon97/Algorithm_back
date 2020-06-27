901e9bfa98f2b0b29af5a3600270c25e44db3b31
devkwon97 <kkm97721@gmail.com>
2020-06-28 01:32:02 +0900

    commit

---
+++ src/baekjoon/Baekjoon2884.java
@@ -0,0 +1,29 @@
+package baekjoon;
+//바로 "45분 일찍 알람 설정하기"이다.
+//
+//이 방법은 단순하다. 원래 설정되어 있는 알람을 45분 앞서는 시간으로 바꾸는 것이다. 어차피 알람 소리를 들으면, 알람을 끄고 조금 더 잘 것이기 때문이다. 이 방법을 사용하면, 매일 아침 더 잤다는 기분을 느낄 수 있고, 학교도 지각하지 않게 된다.
+//
+//현재 상근이가 설정한 알람 시각이 주어졌을 때, 창영이의 방법을 사용한다면, 이를 언제로 고쳐야 하는지 구하는 프로그램을 작성하시오.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon2884 {
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int hour = Integer.parseInt(st.nextToken());
+        int minute = Integer.parseInt(st.nextToken());
+
+        if(minute >= 45){
+            System.out.print(hour+" "+(minute-45));
+        }else{
+            if(hour==0){
+                hour=24;
+            }
+            System.out.print(hour - 1 + " "+ (minute+15));
+        }
+    }
+}
