6216663ef969904e6b61f93978cff72f01269efe
dev_kwon97 <kkm97721@gmail.com>
2020-07-12 22:39:01 +0900

    2020.07.12 commit

--- src/baekjoon/Baekjoon2675.java
+++ src/baekjoon/Baekjoon2675.java
@@ -3,25 +3,28 @@ package baekjoon;
 // 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
 //
 //QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Baekjoon2675 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        int T = Integer.parseInt(br.readLine());
 
-        int T = in.nextInt();
-        for(int i = 0; i < T; i++) {
+        for(int i=0; i<T; i++){
+            String[] str = br.readLine().split(" "); // 공백 분리
 
-            int R = in.nextInt();
-            String S = in.next();	// nextLine() 을 쓰면 error!
+            int R = Integer.parseInt(str[0]);
+            String S = str[1];
 
-            for(int j = 0; j < S.length(); j++) {
-                for(int k = 0; k < R; k++) {	// R 만큼 반복 출력
+            for(int j=0; j<S.length(); j++){
+                for(int k=0; k<R; k++){
                     System.out.print(S.charAt(j));
                 }
             }
-
             System.out.println();
         }
     }
