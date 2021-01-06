5d5ecda3b11d262556dc41c83eb64a2eac0c150d
devkwon97 <dev_kwon97@naver.com>
2021-01-06 23:56:05 +0900

    2447번 별 찍기 문제

---
+++ src/baekjoon_recursion/Beakjoon2447.java
@@ -0,0 +1,13 @@
+package baekjoon_recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beakjoon2447 {
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
