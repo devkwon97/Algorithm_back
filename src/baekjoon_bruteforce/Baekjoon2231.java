bb3f428a4bb1f1c369f6f9d977f71aa3a30433e8
devkwon97 <dev_kwon97@naver.com>
2021-01-11 23:35:59 +0900

    백준 2231 분해합

--- src/baekjoon_bruteforce/Baekjoon2231.java
+++ src/baekjoon_bruteforce/Baekjoon2231.java
@@ -1,4 +1,8 @@
 package baekjoon_bruteforce;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Baekjoon2231 {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 }
