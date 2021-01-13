50104ceaef1e8c340de4693ec6ef2e2860b50b1c
devkwon97 <dev_kwon97@naver.com>
2021-01-13 23:59:02 +0900

    풀이 중

--- src/baekjoon_bruteforce/Baekjoon2231.java
+++ src/baekjoon_bruteforce/Baekjoon2231.java
@@ -1,8 +1,14 @@
 package baekjoon_bruteforce;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Baekjoon2231 {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Baekjoon2231{
+    public void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+
+    }
 }
