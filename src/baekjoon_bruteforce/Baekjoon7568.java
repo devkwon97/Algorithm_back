f23e91abd5db8e199bc7835b425c08535bb029b6
devkwon97 <dev_kwon97@naver.com>
2021-02-12 23:54:23 +0900

    7568 commit

--- src/baekjoon_bruteforce/Baekjoon7568.java
+++ src/baekjoon_bruteforce/Baekjoon7568.java
@@ -1,7 +1,10 @@
 package baekjoon_bruteforce;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Baekjoon7568 {
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
