b149000d44841d4d9ec53567fee65af1b2349996
devkwon97 <dev_kwon97@naver.com>
2021-03-03 23:58:23 +0900

    7568 문제

--- src/baekjoon_bruteforce/Baekjoon7568.java
+++ src/baekjoon_bruteforce/Baekjoon7568.java
@@ -1,10 +1,18 @@
 package baekjoon_bruteforce;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Baekjoon7568 {
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Scanner in = new Scanner(System.in);
+        int Num = in.nextInt();
+        int[][] arr = new int[Num][2];
+
+        for(int i = 0; i < Num; i++) {
+            arr[i][0] = in.nextInt(); // [i][0] 몸무게
+            arr[i][1] = in.nextInt(); // [i][1] 키
+        }
+
     }
 }
